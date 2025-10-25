b36737e4d3a0c6d200934b7b417e1cacb288c3eb
Jeremias Goytia <jeremiasjg99@gmail.com>
2025-10-25 12:43:38 -0300

    lib/supabase changed

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -5,6 +5,6 @@ export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   {
-    auth: { persistSession: true, autoRefreshToken: true, detectSessionInUrl: true }
+    auth: { persistSession: true, autoRefreshToken: false, detectSessionInUrl: true }
   }
 );
