ae3b4d4380055a6a2972893d7e84290efe539142
Jeremias Goytia <jeremiasjg99@gmail.com>
2025-12-11 12:45:47 -0300

    dashboards and maps update

--- components/LookerEmbed.jsx
+++ components/LookerEmbed.jsx
@@ -3,8 +3,8 @@ export default function LookerEmbed({looker_id}) {
   const LOOKER_LINKS = {
     masivos: 'https://lookerstudio.google.com/embed/reporting/ad82bc6b-d9d2-4c2e-b5a7-b761ecbdf7f3/page/zlJiF',
     refrigerados: 'https://lookerstudio.google.com/embed/reporting/c5ac5605-9848-4fa9-9433-0afa6cfc867c/page/p_f38jghr7xd',
-    chaco: 'https://lookerstudio.google.com/embed/reporting/711d1e14-c3f5-4463-868b-346acfd1c035/page/CUieF',
-    misiones: 'https://lookerstudio.google.com/embed/reporting/2be5fc5a-2f9e-49cf-88da-0978009acafa/page/kAmeF',
+    chaco: 'https://lookerstudio.google.com/embed/reporting/95f84fe1-d5b8-4b6d-b877-fe5c4015c135/page/3eLiF',
+    misiones: 'https://lookerstudio.google.com/embed/reporting/306b82d0-7f23-41c8-8950-f57e789d1cc5/page/W9LiF',
     obera: 'https://lookerstudio.google.com/embed/reporting/fbf85486-68a1-4cf8-8b7f-7fa52a2adcc8/page/aUKiF',
     gerencia: 'https://lookerstudio.google.com/embed/reporting/cef0180a-7960-4a53-9866-f390ec75f579/page/yLQfF'
   }
