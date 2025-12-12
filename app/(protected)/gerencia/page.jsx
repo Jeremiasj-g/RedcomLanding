05d8aae0cd5aa37abf9cfc7eb6ef14062ddd99df
Jeremias Goytia <jeremiasjg99@gmail.com>
2025-12-12 10:41:29 -0300

    planillas de criticos añadido en gerencia

--- app/(protected)/gerencia/page.jsx
+++ app/(protected)/gerencia/page.jsx
@@ -12,6 +12,7 @@ import FullScreenEmbedCard from '@/components/FullScreenEmbedCard';
 import { urls } from '@/lib/data';
 import { Table } from 'lucide-react';
 import { RequireAuth } from '@/components/RouteGuards';
+import CardSucursales from '@/components/CardSucursales';
 // (opcional) import { IconMapPoint } from '@/components/Icons/IconMapPoint';
 
 /* const MAP_URL = 'https://www.google.com/maps/d/u/0/embed?mid=19y6MniEXtnVs3QBIZOlaXGOnkRMVTkI&ehbc=2E312F'; */
@@ -31,11 +32,32 @@ export default function Gerencia() {
           bgImage="/mapa-corrientes.png"
         />
 
-        {/* Card + Modal reutilizable */}
         <section className="pt-24 pb-14">
           <Container>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
+              {gerenciaProducts.map((product, index) => (
+                <motion.div
+                  key={product.id}
+                  initial={{ opacity: 0, y: 30 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{
+                    duration: 0.5,
+                    delay: index * 0.1
+                  }}
+                  viewport={{ once: true }}
+                >
+                  <CardSucursales {...product} />
+                </motion.div>
+              ))}
+            </div>
+          </Container>
+        </section>
+
+        {/* Card + Modal reutilizable */}
+        <section className="pb-14">
+          <Container>
             <FullScreenEmbedCard {...gerenciaMapa} />
-            <FullScreenEmbedCard {...gerenciaTablero} icon={<Table />}  />
+            <FullScreenEmbedCard {...gerenciaTablero} icon={<Table />} />
           </Container>
         </section>
         <Container>
