3a1e1ad25bc2f4e6ef9cbf66f9da48e4b6ad0867
Jeremias Goytia <jeremiasjg99@gmail.com>
2026-03-11 09:38:57 -0300

    data.js tableros added

--- components/LookerEmbed.jsx
+++ components/LookerEmbed.jsx
@@ -7,19 +7,19 @@ export default function LookerEmbed({ looker_id, bgImage }) {
 
   const LOOKER_LINKS = {
     masivos:
-      'https://lookerstudio.google.com/embed/reporting/8a84b227-73a0-4465-ab4a-ffc4daf6be49/page/W41nF',
+      'https://lookerstudio.google.com/embed/reporting/76975544-c99b-4cb2-be4c-0e8600ef0a24/page/LwcrF',
     refrigerados:
-      'https://lookerstudio.google.com/embed/reporting/b1165fcb-7d52-4856-9c09-04dfdfe1acf2/page/1f9lF',
+      '',
     refrigeradosKilos:
-      'https://lookerstudio.google.com/embed/reporting/8e7c5cb0-1d73-41e0-b1a2-5279a8a73f2d/page/838nF',
+      '',
     chaco:
-      'https://lookerstudio.google.com/embed/reporting/80c6f92f-61f4-4d70-90c8-41c4b71f9930/page/Ie3nF',
+      '',
     misiones:
-      'https://lookerstudio.google.com/embed/reporting/c657a1d6-8c36-4359-a219-6e92537e175c/page/oJ8nF',
+      '',
     obera:
-      'https://lookerstudio.google.com/embed/reporting/d6f9ac8f-cf36-490d-aa9a-f9c79b317a9a/page/Yc8nF',
+      '',
     gerencia:
-      'https://lookerstudio.google.com/embed/reporting/c800f6e3-844d-4493-8600-5125845d547e/page/7y9nF',
+      '',
   };
 
   const src = useMemo(() => {
