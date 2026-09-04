3a7fe901722d99e18f9080ea6ed7d6b9d56f2763
Jeremiasj-g <120017819+Jeremiasj-g@users.noreply.github.com>
2026-09-04 12:34:13 -0300

    feat: montar historico de periodos CCC

--- app/(protected)/ccc-calificados/page.tsx
+++ app/(protected)/ccc-calificados/page.tsx
@@ -1,10 +1,12 @@
 import ClientesCalificadosPage from "./ClientesCalificadosPage";
 import CccExportAllBrandsFix from "./CccExportAllBrandsFix";
+import CccSnapshotFeature from "./CccSnapshotFeature";
 
 export default function Page(){
   return (
     <>
       <CccExportAllBrandsFix />
+      <CccSnapshotFeature />
       <ClientesCalificadosPage />
     </>
   );
